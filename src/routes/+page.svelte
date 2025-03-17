516d1c48cbfa27231267d158c74518d8d6253a21
ceoDemitri <demichole@gmail.com>
2025-03-16 22:56:45 -0500

    Project kickstart; deployed with vercel having a custom url. basic homepage.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<article class="text-center">
+	<h1 class="text-5xl">Demitri Echols</h1>
+	<p>Software Engineer</p>
+</article>
+
+<ul class="flex gap-6 justify-center mt-16">
+	<li>Web</li>
+	<li>Software</li>
+	<li>Design</li>
+</ul>
