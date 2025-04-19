750eb3b3852a8ece98cf094daab5ba1766486d0c
ceoDemitri <demichole@gmail.com>
2025-04-19 09:27:01 -0500

    add a whale img, one pseudo hero section in the main section of page. New navigation bar and remove the cursor pointer from the heading

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Beautiful gallery of my skills I use to freelance. I am pioneering an evolution.
 
 Lay down the content for the site. What do I need? What's really important? Very little style until I lay the foundation.
 [ ] Completed
+
+- Notes
+  Going with no flow, guessing and hoping. Not complicated, getting me back in the motion. Overall, working to land in a special spot. This will be assistance.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,41 @@
 <article class="text-center">
-	<h1 class="text-5xl">Demitri Echols</h1>
+	<h1 class="text-3xl font-bold">Demitri Echols</h1>
 	<p>Software Engineer</p>
 	<p>Crafting your ideas to the finest precision.</p>
 </article>
 
-<ul class="flex gap-6 justify-center mt-16">
+<ul class="flex gap-6 justify-center">
 	<li>Web</li>
 	<li>Software</li>
 	<li>Design</li>
 </ul>
 
-<a href="/">Resume</a>
-<a href="/">Projects</a>
+<nav class="flex flex-col">
+	<a href="/">Current Resume</a>
+	<a href="/">Examples of my Work</a>
+	<a href="/">Let Us Connect</a>
+</nav>
+
+<main class="my-8">
+	<section>
+		<h2>Many ways to crack a Problem. Try me.</h2>
+		<p>
+			For eons, we as humans looked to nature for inspiration, finding it in least likely places,
+			returning with solid gold. As a developer, I am creating new bridges better aligning nature
+			with technology. How do we make the most of this temporary home, making it better for those
+			after us? Let's go for a dive.
+		</p>
+		<img
+			src="https://images.unsplash.com/photo-1698472505070-6d3b90afb530?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d2hhbGV8ZW58MHx8MHx8fDA%3D"
+			alt="me when I am a whale. img by Chinh Le Duc @ unsplash"
+			class="w-1/2 h-1/5"
+		/>
+	</section>
+</main>
 
 <style>
 	li {
 		color: lightgray;
-		cursor: pointer;
 		position: relative;
 		display: inline-block;
 
